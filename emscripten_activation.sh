03955c4abba3fd13b374469855b67e5170bb76c4
PhilipHersin <80890965+philipivanowh@users.noreply.github.com>
2026-07-19 22:01:02 -0400

    GitHub Pages deploy workflow for web build

--- emscripten_activation.sh
+++ emscripten_activation.sh
@@ -1,3 +1,7 @@
 cd /Users/lansmachine/Documents/Tools/emsdk
 ./emsdk activate latest
 source ./emsdk_env.sh
+
+cd //Users/lansmachine/Documents/Github/FizixLabWebGPU
+cmake --build build-web -j8
+python3 -m http.server 8123 --directory build-web
